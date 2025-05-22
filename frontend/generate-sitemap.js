23dfd7438133a896700f807fa7b3ba583f835e97
KhaMinh <minhkha06122004@gmail.com>
2025-05-22 17:28:45 +0700

    sitemap

---
+++ frontend/generate-sitemap.js
@@ -0,0 +1,28 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { writeFile } from 'fs/promises';
+
+const links = [
+  { url: '/', changefreq: 'weekly', priority: 1.0 },
+  { url: '/filmlist', changefreq: 'weekly', priority: 0.9 },
+  { url: '/cinemalist', changefreq: 'weekly', priority: 0.8 },
+  { url: '/promotion', changefreq: 'weekly', priority: 0.7 },
+  { url: '/movie/676a5a6b5a1a0af36bcda847', changefreq: 'weekly', priority: 0.9 },
+  { url: '/movie/67310a23abf676d312156c6f', changefreq: 'weekly', priority: 0.9 },
+  { url: '/movie/6730afcba7927091fa04dea5', changefreq: 'weekly', priority: 0.9 },
+];
+
+const sitemap = new SitemapStream({ hostname: 'https://ceecine.vercel.app' });
+
+streamToPromise(sitemap)
+  .then((data) => {
+      return writeFile('./sitemap.xml', data.toString());
+    })
+  .then(() => {
+    console.log('✅ Sitemap created!');
+  })
+  .catch(err => {
+    console.error('❌ Error creating sitemap:', err);
+  });
+
+links.forEach(link => sitemap.write(link));
+sitemap.end();
